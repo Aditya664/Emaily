5bc599570b9835c7388aa95136cb44746493b799
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2026-09-19 14:48:11 +0530

    implement Google OAuth authentication with Passport.js and update dependencies

--- server/index.js
+++ server/index.js
@@ -1,15 +1,10 @@
 const express = require("express");
+require("./services/passport");
 const app = express();
+
+require("./routes/authRoutes")(app);
+
 const port = process.env.PORT || 5000;
-
-app.get("/", (req, res) => {
-  res.send({ status: "ok", service: "Emaily server" });
-});
-
-app.get("/hello", (req, res) => {
-  res.send({ hi: "There" });
-});
-
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
